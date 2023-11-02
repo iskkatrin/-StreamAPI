a12cddfe86329e14d767792f302f22649e54bf6b
Ekaterina <klarina2409@gmail.com>
2023-11-02 22:05:12 +0300

    HW

--- src/main/java/com/example/demo/Stream/API/DepartmentController.java
+++ src/main/java/com/example/demo/Stream/API/DepartmentController.java
@@ -1,23 +1,34 @@
 package com.example.demo.Stream.API;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.Map;
+
 public class DepartmentController {
+
     private EmployeeService employeeService;
 
-    public DepartmentController() {
-        employeeService = new EmployeeService();
+    // внедрение зависимости от EmployeeService через конструктор
+
+    @GetMapping("/max-salary")
+    public Employee getEmployeeWithMaxSalaryByDepartment(@RequestParam("departmentId") int departmentId) {
+        return employeeService.getEmployeeWithMaxSalaryByDepartment(departmentId);
     }
 
-    @GetMapping("/departments/max-salary")
-    public ResponseEntity<Employee> getEmployeeWithMaxSalaryByDepartment(@RequestParam("departmentId") int departmentId) {
-        Employee employee = employeeService.getEmployeeWithMaxSalaryByDepartment(departmentId);
-        if (employee != null) {
-            return ResponseEntity.ok().body(employee);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping("/min-salary")
+    public Employee getEmployeeWithMinSalaryByDepartment(@RequestParam("departmentId") int departmentId) {
+        return employeeService.getEmployeeWithMinSalaryByDepartment(departmentId);
+    }
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployeesByDepartment(@RequestParam("departmentId") int departmentId) {
+        return employeeService.getAllEmployeesByDepartment(departmentId);
+    }
+
+    @GetMapping("/all")
+    public Map<Integer, List<Employee>> getAllEmployeesByDepartment() {
+        return employeeService.getAllEmployeesByDepartment();
     }
 }
