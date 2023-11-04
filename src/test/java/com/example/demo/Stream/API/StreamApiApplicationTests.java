9293e18802df9a24b991cffabb44676bce5caa7b
Ekaterina <klarina2409@gmail.com>
2023-11-04 13:13:10 +0300

    HW

---
+++ src/test/java/com/example/demo/Stream/API/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.Stream.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
